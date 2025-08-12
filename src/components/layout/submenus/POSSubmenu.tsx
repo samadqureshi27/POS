baeff0cb0868888f79d3ad9ee7be95e3c65d4777
samadqureshi27 <qabdulsamad18@gmail.com>
2025-08-12 22:38:22 +0500

    final layout

--- src/components/layout/submenus/POSSubmenu.tsx
+++ src/components/layout/submenus/POSSubmenu.tsx
@@ -5,11 +5,8 @@ import BaseSubmenu from '../BaseSubmenu';
 
 export default function POSSubmenu() {
   const items = [
-    // { label: 'POS List', href: '/pos-list' },
     { label: 'Inventory Management', href: '/inventory-management' },
-    { label: 'Staff Management', href: '/staff-management' },
-    // { label: 'Menu Management', href: '/menu-management' },
-    // { label: 'Recipes Management', href: '/recipes-management' },
+    { label: 'Staff Management', href: '/employee-records' },
   ];
 
   return <BaseSubmenu items={items} />;
--- src/components/layout/submenus/StaffSubmenu.tsx
+++ src/components/layout/submenus/StaffSubmenu.tsx
@@ -5,7 +5,6 @@ import BaseSubmenu from '../BaseSubmenu';
 
 export default function StaffSubmenu() {
   const items = [
-    { label: 'Staff Management', href: '/staff-management' },
     { label: 'Employee Records', href: '/employee-records' },
     { label: 'Payroll', href: '/payroll' },
   ];
